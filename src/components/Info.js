729122669f5c0abac222f9736a53ffc72bc56a68
Evgeny <lmbrjk22ak@gmail.com>
2021-03-17 17:30:52 +0700

    dispatch working! 2

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { useSelector, useDispatch } from 'react-redux'
-import { connect } from "react-redux"
 import { Link } from "react-router-dom"
+import { connect } from "react-redux"
+import { useSelector, useDispatch } from 'react-redux'
+
 
 function Info({ match }){ 
     
