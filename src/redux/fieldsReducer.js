fa6b5b876f93bf31b4d95d6d22bd660904dd9901
Evgeny <lmbrjk22ak@gmail.com>
2021-03-22 17:13:38 +0700

    change fieldsReducer

--- src/redux/fieldsReducer.js
+++ src/redux/fieldsReducer.js
@@ -1,7 +1,15 @@
 import {CHANGE_FIELDS_LIST} from "./types"
 
 const initialState = {
-    all: ["name", "middlename", "surname", "birthday", "number", "post", "division"],
+    inputs: [
+        { typeField: "text", nameField: "name", labelField: "Имя", hidden: true },
+        { typeField: "text", nameField: "middlename", labelField: "Отчество", hidden: false },
+        { typeField: "text", nameField: "surname", labelField: "Фамилия", hidden: false },
+        { typeField: "text", nameField: "birthday", labelField: "Дата рождения", hidden: false },
+        { typeField: "text", nameField: "number", labelField: "Табельный номер", hidden: false },
+        { typeField: "text", nameField: "post", labelField: "Должность", hidden: true },
+        { typeField: "text", nameField: "division", labelField: "Подразделение", hidden: false }
+    ],
     hidden: ["id", "post"]
 };
 
