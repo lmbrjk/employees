533f1a13c07fac60b20cf4690b2c4658d9d451b7
Evgeny <lmbrjk22ak@gmail.com>
2021-03-24 16:34:21 +0700

    without redux-form, react-final-form and etc

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -15,47 +15,6 @@ function NewItem() {
     const [inputs] = useState(allInputs.filter(input => input.hidden === false));
 
     const [item, setLocalState] = useState({});
-    
-
-    // submitHandler = event => {
-    //     event.preventDefault();
-
-    //     const {name, middlename, surname, birthday, number, post, division} = this.state;
-
-    //     //небольшая проверка на незаполненные поля
-    //     if(!name.trim() || !middlename.trim() || !surname.trim() || !number.trim() || !post.trim() || !division.trim()){
-    //         return
-    //     }
-
-    //     const newItem = {
-    //         id: Date.now().toString(),
-    //         name, middlename, surname, birthday, number, post, division
-    //     }
-
-    //     //изменяем state
-    //     this.props.createItem(newItem);
-
-    //     this.setState({
-    //         name: "",
-    //         middlename: "",
-    //         surname: "",
-    //         birthday: "",
-    //         number: "",
-    //         post: "",
-    //         division: ""            
-    //     })
-        
-    // }
-
-    // const changeInput = (event) => {       
-    //     setLocalInputs( 
-    //         item => (
-    //             {...item, 
-    //             ...{[event.target.name]: event.target.value}
-    //             }
-    //         )
-    //     );
-    // }
 
     const changeInput = (event) => { 
         setLocalState(
